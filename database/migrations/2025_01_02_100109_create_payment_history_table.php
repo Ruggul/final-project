b137d6522b1ddd7421734ad5350cac9eb010a02d
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2025-01-02 17:02:48 +0700

    create payment history

---
+++ database/migrations/2025_01_02_100109_create_payment_history_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payment_histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number')->unique();
+            $table->decimal('amount', 12, 2);
+            $table->enum('payment_status', ['pending', 'paid', 'failed', 'refunded']);
+            $table->enum('payment_method', ['credit_card', 'bank_transfer', 'e-wallet', 'other']);
+            $table->date('payment_date');
+            $table->date('due_date');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_histories');
+    }
+};
