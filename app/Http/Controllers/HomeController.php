b9f1f53745273bcda349f105fdfc91fcaa4f7781
Ruggul <cacanz.canz@gmail.com>
2025-01-02 14:22:06 +0700

    update view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function redirect()
     {
         $usertype=Auth::user()->usertype;
+
         if($usertype=='1'){
             return view('admin.home');
-        }elseif($usertype=='2') {
+        }elseif($usertype=='2'){
             return view('factory.home');
         }else{
             return view('dashboard');
