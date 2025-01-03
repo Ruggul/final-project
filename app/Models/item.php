2a0d4f805152c1f0c8dba60a083fae28edd74546
Parvisyeah <148097695+Parvisyeah@users.noreply.github.com>
2025-01-03 21:15:13 +0700

    commmit

--- app/Models/item.php
+++ app/Models/item.php
@@ -9,21 +9,14 @@ class Item extends Model
     protected $fillable = [
         'kode_barang',
         'nama_barang',
-        'deskripsi',
         'stok',
         'satuan',
         'harga_satuan',
-        'lokasi_penyimpanan',
         'category_id'
     ];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
-    }
-
-    public function stockMovements()
-    {
-        return $this->hasMany(StockMovement::class);
     }
 }
