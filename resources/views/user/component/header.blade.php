8d51994f611e70ad667f269d57b3c733bff897c7
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2025-01-04 03:41:18 +0700

    Update header.blade.php

--- resources/views/user/component/header.blade.php
+++ resources/views/user/component/header.blade.php
@@ -32,7 +32,7 @@
                     </div>
 
                     <a href="{{ route('profile.show') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                        {{ __('Profile') }}
+                        {{ __('TopUp') }}
                     </a>
 
                     <!-- Authentication -->
