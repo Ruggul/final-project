dfcce20860c3f3c4252b8f342bfcbb9da489daf7
Dhafin06 <aksanidradhafin05@gmail.com>
2025-01-03 15:27:18 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
+
+                <!-- Button to Cart Management -->
+                <div class="mt-4">
+                    <a href="{{ route('cart') }}"
+                        class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                        Go to Cart Management
+                    </a>
+                </div>
             </div>
         </div>
     </div>
